5154540460c79c549a95a891e6ac0a6327cac60d
Bob Moriasi <bob.b.moriasi.26@dartmouth.edu>
2023-03-26 19:22:11 -0400

    Initial Commit

---
+++ Audio Journal/Audio_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Audio_JournalApp.swift
+//  Audio Journal
+//
+//  Created by Bob Moriasi on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Audio_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
